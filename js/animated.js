6a46635ce43536796dfa355024d01fdafb9c3af1
Max <97470301+Max86634@users.noreply.github.com>
2024-04-11 22:23:35 +0300

    Add files via upload
    upadate

---
+++ js/animated.js
@@ -0,0 +1,16 @@
+const tl = gsap.timeline({ defaults: { duration: 1.35}});
+const tl2 = gsap.timeline({ defaults: {duration: .35}});
+
+tl.from('.billboard-icon', { opacity: 0 , x: 300})
+  tl2.from('.t1', { opacity: 0 , y: 30})
+  .from('.t2', { opacity: 0 , y: 30})
+  .from('.t3', { opacity: 0 , y: 30})
+  .from('.t4', { opacity: 0 , y: 30})
+  .from('.t5', { opacity: 0 , y: 30})
+  .from('.t6', { opacity: 0 , y: 30})
+tl.from('.billboard-text', { opacity:0, y: -30})
+tl.from('.showcase__header', {opacity: 0, y: 30});
+
+tl2.from('head-text', { opacity: 0, y: -30});
+tl.from('.main-text', { opacity: 0, y: 30});
+
