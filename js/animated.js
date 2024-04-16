df20c41b192b748d4c56889479fd328830d64a4d
Max <97470301+Max86634@users.noreply.github.com>
2024-04-16 22:42:40 +0300

    Add files via upload
    update

---
+++ js/animated.js
@@ -0,0 +1,39 @@
+const tl = gsap.timeline({ defaults: { duration: .35}});
+const tl2 = gsap.timeline({ defaults: {duration: .75}});
+
+tl.from('.billboard-text', { opacity:0, y: -30})
+tl2.from('.billboard-icon', { opacity: 0 , x: 300})
+  tl.from('.t1', { opacity: 0 , y: 30})
+  .from('.t2', { opacity: 0 , y: 30})
+  // .from('.t3', { opacity: 0 , y: 30})
+  // .from('.t4', { opacity: 0 , y: 30})
+  .from('.t5', { opacity: 0 , y: 30})
+  .from('.t6', { opacity: 0 , y: 30})
+// tl.from('.showcase__header', {opacity: 0, y: 30});
+
+let container = () =>{
+  const contAnimate = gsap.timeline({ defaults: {duration: 1}, repeat: -1, repeatDelay: 1});
+
+  tl2.from('.n1', { opacity: 0, y: -30});
+  tl2.from('.n1', { opacity: 1, y: 30});
+  tl2.from('.n2', { opacity: 0, y: -30});
+  tl2.from('.n2',  { opacity: 1, y: 30});
+  return contAnimate;
+}
+
+tl2.from('head-text', { opacity: 0, y: -30});
+tl2.from('.main-text', { opacity: 0, y: 30});
+tl2.add(container);
+
+
+
+
+
+
+
+
+
+
+
+
+
