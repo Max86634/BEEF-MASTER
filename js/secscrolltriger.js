5b4a5bcc0f3a5d2e17126c5d8d5feb3572f34d82
Max <97470301+Max86634@users.noreply.github.com>
2024-04-19 21:44:52 +0300

    Add files via upload
    update

---
+++ js/secscrolltriger.js
@@ -0,0 +1,17 @@
+// Подключаем ScrollTrigger из библиотеки GSAP
+gsap.registerPlugin(ScrollTrigger);
+
+// Создаем анимацию для элементов с классом ".recipe-box"
+gsap.utils.toArray('.recipe-box').forEach(box => {
+  gsap.from(box, {
+    opacity: 0,
+    y: 50,
+    duration: 1.5,
+    scrollTrigger: {
+      trigger: box,
+      start: 'top 80%', // Начать анимацию, когда элемент появится на 80% от верха окна
+      end: 'bottom 50%', // Завершить анимацию, когда элемент окажется на 20% от низа окна
+      toggleActions: 'play none none none' // Воспроизвести анимацию при прокрутке вниз, но не останавливать ее при обратной прокрутке
+    }
+  });
+});
